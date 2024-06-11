5f72e5d6507e1964f437c6b9faf24d9bbff75a7c
sltech1-admin <stein@sltech.no>
2024-06-11 02:56:10 +0200

    removes comments

--- Program.cs
+++ Program.cs
@@ -7,35 +7,3 @@ new Config(args,
     new SSL(),
     new Service(typeof(ITheRepo), typeof(TheRepo), ServiceScopes.Transient),
     new DefaultRoute("Demo", "Index"));
-
-
-
-/*
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
-*/
